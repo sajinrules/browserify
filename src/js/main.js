2b51c855665914ac79d15d6d9616c872fee92f0c
root <root@t016.beo.in>
2015-11-23 18:22:24 +0530

    rect router | route configuration

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,11 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 var NavBar = require('./components/NavBar.jsx');
+var Login = require('./components/Login.jsx');
 var Router = require('react-router').Router
 var Route = require('react-router').Route
 var Link = require('react-router').Link
+var IndexRoute =  require('react-router').IndexRoute
 injectTapEventPlugin = require("react-tap-event-plugin");
 
 //Needed for onTouchTap
@@ -15,6 +17,10 @@ injectTapEventPlugin();
 ReactDOM.render(
 	(
 		<Router>
-			<Route path="/" component={NavBar}></Route>
+			<Route path="/" component={NavBar}>
+			<IndexRoute component={Login} />
+				
+			</Route>
+			
 		</Router>
 	), document.getElementById('main'));
